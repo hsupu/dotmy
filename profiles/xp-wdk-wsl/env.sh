81cee9b3fd73fa1262c8e0a03f544ae5dec806bd
Hsu Pu <hsupu@msn.com>
2022-11-17 05:08:58 +0800

    set EL=LF

---
+++ profiles/xp-wdk-wsl/env.sh
@@ -0,0 +1,12 @@
+
+export DOTMY="$HOME/.config/dotmy"
+[[ -d $DOTMY ]] || echo "\$DOTMY not found: $DOTMY"
+
+export DOTMY_PROFILE="xp-wdk-wsl"
+
+# pyenv
+export PYENV_ROOT="$HOME/.local/pyenv"
+# nvm
+export NVM_DIR="$HOME/.local/nvm"
+# pnpm
+export PNPM_HOME="$HOME/.local/share/pnpm"
